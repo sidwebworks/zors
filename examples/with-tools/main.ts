981f9dc970842e993a5f904f9c8bcc219c4da07f
Sidharth Rathi <Xodedeveloper@gmail.com>
2022-07-01 14:02:56 +0530

    feat: add with-tools example

--- examples/with-tools/main.ts
+++ examples/with-tools/main.ts
@@ -1,7 +1,14 @@
-import { zors } from 'zors';
-import { commitCommand } from './commands.js';
+import { defineCommand, zors } from 'zors';
+import { logger } from './utils/logger.js';
+import colors from "picocolors"
 
-const program = zors('git', '1.0.0');
+// Add your tools inside config
+const program = zors('git', '1.0.0', {
+  tools: {
+    logger,
+    pico: colors
+  },
+});
 
 program
   .help() // Show help
@@ -11,20 +18,39 @@ program
   )
   .option('-c, --commit', 'stage files and create an initial commit')
   .example('init --no-commit')
-  .action((_, { commit }) => {
-    console.log(`Intialized an empty git repository`);
+  .action((_, { commit }, { logger ,pico}) => {
+    logger.info(pico.bgGreen(pico.black(`Intialized an empty git repository`)));
     if (commit) {
-      console.log(`Created an initial commit`);
+      logger.info(`Created an initial commit`);
     }
-  })
-  .command<string[]>('add <...files>', 'Add files to track')
-  .action((files) => {
-    if (files.length === 1 && files[0] === '.') {
-      console.log(`Tracking all files`);
-    } else {
-      console.log(`Tracking files: `, files);
-    }
-  })
-  .register(commitCommand);
+  });
 
-program.run(process.argv.slice(2));
+export const commitCommand = defineCommand<undefined, { message: string }>(
+  'commit',
+  {
+    description: 'Creates a commit with a given message',
+    options: [
+      { raw: '-m, --message <message>', description: 'Commit message' },
+    ],
+    examples: ["commit -m 'chore: update packages'"],
+    usage: 'commit -m <commit message>',
+    action(_, { message }, { logger,pico }) {
+      logger.info(pico.red(`Created a commit: ${pico.bgCyan(message)}`));
+    },
+  }
+);
+
+export const addCommand = defineCommand<string[]>('add <...files>', {
+  description: 'Add files to track changes',
+  examples: ['add styles.css app.js index.html', 'add .'],
+  usage: 'add <...list of files>',
+  action(files, _, { logger }) {
+    if (files.length === 1 && files[0] === '.') {
+      logger.info(`Tracking all files`);
+    } else {
+      logger.info(`Tracking files: `, files);
+    }
+  },
+});
+
+program.register(commitCommand).register(addCommand).run(process.argv.slice(2));
---
+++ examples/with-tools/utils/logger.ts
@@ -0,0 +1,30 @@
+import pino from 'pino';
+import dayjs from 'dayjs';
+import { PinoPretty } from 'pino-pretty';
+import colors from "picocolors"
+
+// Extend tools type declaration with your custom tools
+declare module "zors" {
+  interface Tools {
+    logger: typeof logger
+    pico: typeof colors
+  }
+}
+
+const prettyConfig: PinoPretty.PrettyOptions = {
+  colorize: true,
+  levelFirst: true,
+};
+
+export const logger = pino({
+  transport: {
+    target: 'pino-pretty',
+    options: prettyConfig,
+  },
+  base: {
+    pid: false,
+  },
+  timestamp: () => `, "time": "${dayjs().format()}"`,
+});
+
+
